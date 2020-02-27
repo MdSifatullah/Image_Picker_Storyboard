d179bca79aac1c83f21e1df45b13c3c739a9473f
Md Sifat <cse.sifat@gmail.com>
2020-02-27 15:03:28 +0600

    Using storyboard image pick from gallery

--- Image Picker/ViewController.swift
+++ Image Picker/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var imagePicker = UIImagePickerController()
 
+
+    @IBOutlet weak var imageVIew: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        imagePicker.delegate = self
     }
 
 
+    @IBAction func onClickBtn(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = true
+        present(imagePicker, animated: true, completion: nil)
+    }
 }
 
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            imageVIew.image = image
+            
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
